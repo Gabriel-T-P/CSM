ed14f75e41432419a2cc6c111ef157f90fbd3459
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-05-15 14:06:19 -0300

    user model updated and register form added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,13 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   around_action :switch_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :first_name, :last_name])
+  end
 
   def switch_locale(&action)
     locale = params[:locale] || I18n.default_locale
--- spec/system/user/user_register_account_spec.rb
+++ spec/system/user/user_register_account_spec.rb
@@ -7,6 +7,26 @@ describe 'User creates account', type: :system do
       click_on 'Sign up'
     end
 
-    expect(current_path).to eq new_user_session_path  
+    expect(current_path).to eq new_user_registration_path
+    expect(page).to have_content 'Sign up'
+  end
+
+  it 'successfully' do
+    visit new_user_registration_path
+    fill_in 'Name',	with: 'Test Name'
+    fill_in 'Last Name',	with: 'Test Last Name'
+    fill_in 'Username',	with: 'Test Username'
+    fill_in 'Email',	with: 'email@test.com'
+    fill_in 'Password',	with: '123test'
+    fill_in 'Password Confirmation', with: '123test'
+    click_on 'Register'
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'Welcome! You have signed up successfully'
+    within 'nav' do
+      expect(page).to have_button 'Log out'
+      expect(page).not_to have_link 'Sign up'
+      expect(page).to have_content 'Test Username'
+    end
   end
 end
