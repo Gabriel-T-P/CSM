d7b6433b2fd03549257d535734668d1784cad8a5
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-05-16 16:17:44 -0300

    Added language change button on navbar and added URL translation

--- spec/system/visit_home_spec.rb
+++ spec/system/visit_home_spec.rb
@@ -15,3 +15,26 @@ describe 'Visit home teste js', type: :system, js: true do
     expect(page).to have_content 'Hello World!'
   end
 end
+
+describe 'Guest changes app language', type: :system do
+  it 'by navbar' do
+    visit root_path
+
+    within 'nav' do
+      expect(page).to have_css('.language-button')
+    end
+  end
+
+  it 'successfully' do
+    visit root_path
+    within 'nav' do
+      find('.language-button').click
+      click_on 'Português'
+    end
+
+    within 'nav' do
+      expect(page).to have_link 'ENTRAR'
+      expect(page).not_to have_link 'LOG IN'
+    end
+  end
+end
