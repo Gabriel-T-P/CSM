53ea39ff0bf3653b24c69205ef9a6e4aebf97947
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-05-17 13:22:29 -0300

    CSS added to Login page and translations added too. Added uniqueness to username on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :first_name, :last_name, :username, presence: true
+  validates :username, uniqueness: { case_sensitive: false }
 end
