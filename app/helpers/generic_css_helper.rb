1b2ff17a267df202958a147d6a342f98ae7afc25
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-05-17 21:46:18 -0300

    Added test and CSS to log out button on navbar and refactored some CSS helpers code

--- app/helpers/generic_css_helper.rb
+++ app/helpers/generic_css_helper.rb
@@ -4,4 +4,20 @@ module GenericCssHelper
       class: "text-2xl font-semibold text-center text-gray-800 mb-6"
     }.merge(options)
   end
+
+  def outline_link_to(text, path, color: 'gray', html_options: {})
+    base_class = "px-4 py-2 mr-4 font-medium text-#{color}-800 border-2 border-#{color}-800 rounded-none " \
+                "hover:border-#{color}-400 hover:text-#{color}-400 transition bg-transparent"
+
+    html_options[:class] = [html_options[:class], base_class].compact.join(' ')
+    link_to text, path, html_options
+  end
+
+  def outline_button_to(text, path, color: 'gray', method: :post, html_options: {})
+    base_class = "px-4 py-2 mr-4 font-medium text-#{color}-800 border-2 border-#{color}-800 rounded-none " \
+                "hover:border-#{color}-400 hover:text-#{color}-400 transition bg-transparent"
+
+    html_options[:class] = [html_options[:class], base_class].compact.join(' ')
+    button_to text, path, html_options.merge(method: method)
+  end
 end
