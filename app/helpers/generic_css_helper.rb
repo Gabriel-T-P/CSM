4711b7cf507c9b55e6f2698733cf83b5bf3badd8
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-05-17 22:35:31 -0300

    rubocop offenses fix

--- app/helpers/generic_css_helper.rb
+++ app/helpers/generic_css_helper.rb
@@ -5,19 +5,19 @@ module GenericCssHelper
     }.merge(options)
   end
 
-  def outline_link_to(text, path, color: 'gray', html_options: {})
+  def outline_link_to(text, path, color: "gray", html_options: {})
     base_class = "px-4 py-2 mr-4 font-medium text-#{color}-800 border-2 border-#{color}-800 rounded-none " \
                 "hover:border-#{color}-400 hover:text-#{color}-400 transition bg-transparent"
 
-    html_options[:class] = [html_options[:class], base_class].compact.join(' ')
+    html_options[:class] = [ html_options[:class], base_class ].compact.join(" ")
     link_to text, path, html_options
   end
 
-  def outline_button_to(text, path, color: 'gray', method: :post, html_options: {})
+  def outline_button_to(text, path, color: "gray", method: :post, html_options: {})
     base_class = "px-4 py-2 mr-4 font-medium text-#{color}-800 border-2 border-#{color}-800 rounded-none " \
                 "hover:border-#{color}-400 hover:text-#{color}-400 transition bg-transparent"
 
-    html_options[:class] = [html_options[:class], base_class].compact.join(' ')
+    html_options[:class] = [ html_options[:class], base_class ].compact.join(" ")
     button_to text, path, html_options.merge(method: method)
   end
 end
