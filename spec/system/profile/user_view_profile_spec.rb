3ae86c25ab9c30184cec1c022883248dba3c8499
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-05-23 19:07:22 -0300

    Added one more information to user profile and more test scenarios

--- spec/system/profile/user_view_profile_spec.rb
+++ spec/system/profile/user_view_profile_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'User views own profile page', type: :system do
+describe 'User views profile page', type: :system do
   it 'by navbar' do
     user = create(:user, username: 'Test1')
 
@@ -15,10 +15,34 @@ describe 'User views own profile page', type: :system do
     expect(page).to have_content "Personal Informations"
   end
 
-  it 'and have profile informations' do
+  it 'and have own profile informations' do
     user = create(:user, first_name: 'User', last_name: 'Test', email: 'user_1@email.com', location: 'Brazil', gender: :male, birth_date: 20.years.ago)
 
     login_as user
+    visit profile_path(username: user.username)
+
+    expect(page).to have_content 'Full Name: User Test'
+    expect(page).to have_content 'Email: user_1@email.com'
+    expect(page).to have_content 'Location: Brazil'
+    expect(page).to have_content 'Age: 20 years'
+    expect(page).to have_content 'Pronoun: he/him'
+    expect(page).to have_content 'Member since 2025'
+  end
+
+  it 'and its not authenticated' do
+    user = create(:user, first_name: 'User', last_name: 'Test', email: 'user_1@email.com', location: 'Brazil', gender: :male, birth_date: 20.years.ago)
+
+    visit profile_path(username: user.username)
+
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
+  end
+
+  it 'from another user with success' do
+    user = create(:user, first_name: 'User', last_name: 'Test', email: 'user_1@email.com', location: 'Brazil', gender: :male, birth_date: 20.years.ago)
+    other_user = create(:user)
+
+    login_as other_user
     visit profile_path(username: user.username)
 
     expect(page).to have_content 'Full Name: User Test'
