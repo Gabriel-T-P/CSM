7bf3a53ca5ffdefa359513280b99fad9a374cb14
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-06-16 15:24:47 -0300

    Added translations to the dashboard page

--- spec/system/dashboard/user_view_dashboard_spec.rb
+++ spec/system/dashboard/user_view_dashboard_spec.rb
@@ -24,4 +24,24 @@ describe 'User view own dashboard', type: :system do
     expect(page).to have_content 'My Collections'
     expect(page).to have_content 'My Friends'
   end
+
+  it 'and its not authenticated' do
+    user = create(:user)
+
+    visit user_dashboard_path(user)
+
+    expect(current_path).to eq new_user_session_path
+    expect(page).to have_content 'You need to sign in or sign up before continuing'
+  end
+
+  it 'and its not the owner of the dashboard' do
+    user = create(:user)
+    other_user = create(:user)
+
+    login_as other_user
+    visit user_dashboard_path(user)
+
+    expect(current_path).to eq root_path
+    expect(page).to have_content 'You can not access this page'
+  end
 end
