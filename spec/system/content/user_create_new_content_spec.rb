3441cec481b812257d28315c33307bcd02b6bedc
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-06-22 20:11:11 -0300

    Changed CSS on new content page and fixed error on content system test

--- spec/system/content/user_create_new_content_spec.rb
+++ spec/system/content/user_create_new_content_spec.rb
@@ -22,10 +22,13 @@ describe 'User creates new content', type: :system do
     find(:css, "#content_body", visible: false).set('Test Text in rich text')
     select 'Private', from: 'Visibility'
     click_on 'Choose Tags'
-    click_on 'Test 1'
-    click_on 'Test 2'
+    check 'Test 1'
+    check 'Test 2'
     click_on 'Confirm'
     attach_file(Rails.root.join("spec/support/files/test_avatar.png"))
     click_on 'Create Content'
+
+    expect(current_path).to eq user_dashboard_path(user)
+    expect(page).to have_content 
   end
 end
