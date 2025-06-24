a8f3e03db6670a26ac8e40c0e312a2d09c75aa9b
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-06-23 22:17:31 -0300

    Added some style to dashboard contents and translations

--- spec/system/content/user_create_new_content_spec.rb
+++ spec/system/content/user_create_new_content_spec.rb
@@ -53,4 +53,27 @@ describe 'User creates new content', type: :system do
     expect(page).to have_content 'Username_1'
     expect(page).to have_content time_ago_in_words(Content.first.created_at)
   end
+
+  it 'and clicks on cancel' do
+    user = create(:user, username: 'Username_1')
+    create(:tag, name: 'Test 1')
+    create(:tag, name: 'Test 2')
+
+    login_as user
+    visit user_dashboard_path(user)
+    click_on 'Upload'
+    fill_in 'Title', with: 'Test Title'
+    select 'Private', from: 'Visibility'
+    click_on 'Show Tags'
+    check 'Test 1'
+    check 'Test 2'
+    attach_file(Rails.root.join("spec/support/files/test_avatar.png"))
+    click_on 'Cancel'
+
+    expect(current_path).to eq user_dashboard_path(user)
+    expect(page).not_to have_content 'Content upload with success'
+    expect(page).not_to have_content 'Test Title'
+    expect(page).not_to have_content 'Username_1'
+    expect(page).not_to have_content time_ago_in_words(Content.first.created_at)
+  end
 end
