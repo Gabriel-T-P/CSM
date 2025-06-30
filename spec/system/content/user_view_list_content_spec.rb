3ac7882f4c229e7424f7b0aa014521a1d902c479
Gabriel-T-P <gabrielthiago2000@hotmail.com>
2025-06-30 16:01:05 -0300

    added more test to index content page

--- spec/system/content/user_view_list_content_spec.rb
+++ spec/system/content/user_view_list_content_spec.rb
@@ -40,4 +40,34 @@ describe 'User view list of own contents', type: :system do
     expect(page).to have_link 'Content 2'
     expect(page).not_to have_content 'Not my content'
   end
+
+  it 'and its not authenticated' do
+    user = create(:user)
+    create(:content, title: 'Content 1', user: user)
+
+    visit user_contents_path(user)
+
+    expect(current_path).to eq new_user_session_path
+  end
+
+  it 'and its not the owner of the contents' do
+    user = create(:user)
+    other_user = create(:user)
+    create(:content, title: 'Content 1', user: user)
+
+    login_as other_user
+    visit user_contents_path(user)
+
+    expect(current_path).to eq user_dashboard_path(other_user)
+    expect(page).to have_content 'You can not access this page'
+  end
+
+  it 'and there is no content created' do
+    user = create(:user)
+
+    login_as user
+    visit user_contents_path(user)
+
+    expect(page).to have_content "You haven't created any content yet"
+  end
 end
